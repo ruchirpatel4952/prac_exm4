52d341a3b029f440e741edbdf2923d0356872596
Ruchir <ruchirpatel2000@gmail.com>
2023-10-26 13:54:19 +1030

    fourth: updated spot.h

--- Spot.h
+++ Spot.h
@@ -1,19 +1,19 @@
-#include <tuple>
+
 #ifndef SPOT_H
 #define SPOT_H
+#include <tuple>
 class Spot {
 private:
     std::tuple<int, int> location;
     char category;
 
 public:
-    // Constructor
     Spot(int x, int y, char category) : location(x, y), category(category) {}
     virtual ~Spot(){}
+
     std::tuple<int, int> getLoc() const { return location; }
     char getCategory() const { return category; }
 
-    // Setters
     void setLoc(int x, int y) { location = std::make_tuple(x, y); }
     void setCategory(char category_val) { category = category_val; }
 };
